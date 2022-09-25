0300fef2c32ff3bcb6f5412f5daec23038599a0f
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-26 00:09:52 +0530

    changes

--- app/src/main/java/com/example/project_demo/TabLayout.kt
+++ app/src/main/java/com/example/project_demo/TabLayout.kt
@@ -9,26 +9,45 @@ import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class TabLayout : AppCompatActivity() {
     lateinit var tabLayout: TabLayout
-    lateinit var tabItem: TabItem
+    private lateinit var tabItem: TabItem
     lateinit var tabItem1: TabItem
     lateinit var tabItem2: TabItem
-    lateinit var tabItem3: TabItem
+    private lateinit var tabItem3: TabItem
     lateinit var tabItem4: TabItem
     lateinit var viewPager : ViewPager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab_layout)
         tabLayout = findViewById(R.id.tablayout)
-        tabItem = findViewById(R.id.first)
-        tabItem1 = findViewById(R.id.second)
-        tabItem2 = findViewById(R.id.third)
-        tabItem3 = findViewById(R.id.founth)
-        tabItem4 = findViewById(R.id.five)
+        /*tabItem = findViewById(R.id.first)*/
+        /*tabItem1 = findViewById(R.id.second)*/
+        /*tabItem2 = findViewById(R.id.third)*/
+        /*tabItem3 = findViewById(R.id.fourth)*/
+       /* tabItem4 = findViewById(R.id.five)*/
         viewPager = findViewById(R.id.viewPager)
+
         val fragmentAdapter = FragmentAdapter(this,supportFragmentManager,5)
         viewPager.adapter = fragmentAdapter
 
-        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+        tabLayout.addOnTabSelectedListener(object : TabLayout.BaseOnTabSelectedListener<TabLayout.Tab?> {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
+                if (tab.position == 0 || tab.position == 1 || tab.position == 2 || tab.position == 3 || tab.position == 4){
+                    fragmentAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+
+        /*tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 viewPager.currentItem = tab!!.position
                 if (tab.position == 0 || tab.position == 1 || tab.position == 2 || tab.position == 3 || tab.position == 4){
@@ -43,9 +62,8 @@ class TabLayout : AppCompatActivity() {
             override fun onTabReselected(tab: TabLayout.Tab?) {
 
             }
-        })
+        })*/
 
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
 
     }
 }
